54e80637db03f62f3c9890ce1b4c31eb15417813
panumasw <panumasw1984@gmail.com>
2022-12-05 21:59:08 +0700

    update mern

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors')
 
 //Define ROute Const
 const Auth = require('./routes/api/auth');
@@ -11,9 +12,14 @@ const app = express();
 
 //Middleware
 app.use(express.json({ extended: false }));
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+app.use(cors(corsOptions));
 
 //Connect Database
 connectDB();
+
 
 app.get('/', (req, res) => {
   res.send('API Running');
